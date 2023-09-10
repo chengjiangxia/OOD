27678cccdd24b8ac99fd653cd80462fffe133720
chengjiangxia <98351177+chengjiangxia@users.noreply.github.com>
2023-09-09 21:06:53 -0400

    Finish Suit

--- DeckOfCards/src/DeckOfCards.java
+++ DeckOfCards/src/DeckOfCards.java
@@ -1,3 +1,21 @@
 public class DeckOfCards {
-    
+    public enum Suit {
+        Club (0), Diamond (1), Heart(2), Spade(3);
+        private int value;
+
+        private Suit(int v) {
+            value = v;
+        }
+        public int getValue() {
+            return value;
+        }
+        public static Suit getSuitFromValue(int value) {
+            for (Suit suit : Suit.values()) {
+                if (suit.getValue() == value) {
+                    return suit;
+                }
+            }
+            throw new IllegalArgumentException("No suit with value" + value);
+        }
+    }
 }
